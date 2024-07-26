49c3b32494f2c550543080c635b5db6a6c022498
Mohamedahmedsleem-CS <mohamedahmedsleem29511@gmail.com>
2024-07-26 06:44:02 +0300

    Recent Project

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+
 // import Footer from "@/components/Footer";
 // import Clients from "@/components/Clients";
 // import Approach from "@/components/Approach";
 // import Experience from "@/components/Experience";
-// import RecentProjects from "@/components/RecentProjects";
+
 import { FloatingNav } from "@/components/ui/FloatingNav";
 
 const Home = () => {
@@ -18,7 +20,8 @@ const Home = () => {
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
-        {/* <RecentProjects />
+        <RecentProjects />
+        {/* 
         <Clients />
         <Experience />
         <Approach />
